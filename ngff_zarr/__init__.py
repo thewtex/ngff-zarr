769dfe60d6124cdb76d0f3facdc21a6244f0694c
Matt McCormick <matt.mccormick@kitware.com>
2022-10-03 22:47:30 -0400

    STYLE: __init__.py remove trailing whitespace

--- ngff_zarr/__init__.py
+++ ngff_zarr/__init__.py
@@ -7,7 +7,7 @@ from .itk_image_to_ngff_image import itk_image_to_ngff_image
 from .__about__ import __version__
 from .ngff_image import NgffImage
 from .to_multiscales import to_multiscales, Multiscales
-from .to_ngff_zarr import to_ngff_zarr 
+from .to_ngff_zarr import to_ngff_zarr
 from .methods import Methods
 
 __all__ = [
