3d1dd1eb38246ff68e5579ccad60c86e3c81f2e6
Matt McCormick <matt.mccormick@kitware.com>
2022-11-23 19:56:56 -0500

    BUG: Support multicomponent itk image conversion

--- test/test_itk_image_to_ngff_image.py
+++ test/test_itk_image_to_ngff_image.py
@@ -18,6 +18,19 @@ def test_2d_itk_image(input_images):
     assert ngff_image.name == "image"
     assert ngff_image.axes_units == None
 
+def test_2d_rgb_itk_image(input_images):
+    array = np.random.randint(0, 255, size=(224, 224, 3), dtype=np.uint8)
+    itk_image = itk.image_from_array(array, is_vector=True)
+    ngff_image = itk_image_to_ngff_image(itk_image)
+    assert np.array_equal(np.asarray(itk_image), array)
+    assert ngff_image.dims == ("y", "x", "c")
+    assert ngff_image.scale["x"] == 1.0
+    assert ngff_image.scale["y"] == 1.0
+    assert ngff_image.translation["x"] == 0.0
+    assert ngff_image.translation["y"] == 0.0
+    assert ngff_image.name == "image"
+    assert ngff_image.axes_units == None
+
 def test_2d_itkwasm_image(input_images):
     itk_image = itk.imread(test_data_dir / "input" / "cthead1.png")
     itk_image_dict = itk.dict_from_image(itk_image)
