107b75bfd699b86fcaaa547ea6022aa10ba733e1
Matt McCormick <matt.mccormick@kitware.com>
2023-03-24 17:02:28 -0400

    COMP: Use default_factory for config cache_store
    For:
    
    ```
    ______________________________________ ERROR collecting test/test_to_ngff_zarr_itk.py _______________________________________
    test/test_to_ngff_zarr_itk.py:1: in <module>
        from ngff_zarr import Methods, to_ngff_zarr, to_multiscales
    ngff_zarr/__init__.py:6: in <module>
        from .from_ngff_zarr import from_ngff_zarr
    ngff_zarr/from_ngff_zarr.py:9: in <module>
        from .to_multiscales import Multiscales
    ngff_zarr/to_multiscales.py:24: in <module>
        from .config import config
    ngff_zarr/config.py:23: in <module>
        @dataclass
    ../../bin/mambaforge/envs/ngff-zarr/lib/python3.11/dataclasses.py:1221: in dataclass
        return wrap(cls)
    ../../bin/mambaforge/envs/ngff-zarr/lib/python3.11/dataclasses.py:1211: in wrap
        return _process_class(cls, init, repr, eq, order, unsafe_hash,
    ../../bin/mambaforge/envs/ngff-zarr/lib/python3.11/dataclasses.py:959: in _process_class
        cls_fields.append(_get_field(cls, name, type, kw_only))
    ../../bin/mambaforge/envs/ngff-zarr/lib/python3.11/dataclasses.py:816: in _get_field
        raise ValueError(f'mutable default {type(f.default)} for field '
    E   ValueError: mutable default <class 'zarr.storage.DirectoryStore'> for field cache_store is not allowed: use default_factory
    ```

--- ngff_zarr/config.py
+++ ngff_zarr/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from pathlib import Path
 from platformdirs import user_cache_dir
@@ -10,8 +10,8 @@ if dask.config.get('temporary-directory') is not None:
     _store_dir = dask.config.get('temporary-directory')
 else:
     _store_dir = Path(user_cache_dir('ngff-zarr'))
-default_cache_store = zarr.storage.DirectoryStore(_store_dir,
-            dimension_separator='/')
+def default_store_factory():
+    return zarr.storage.DirectoryStore(_store_dir, dimension_separator='/')
 
 try:
     import psutil
@@ -24,6 +24,6 @@ except ImportError:
 class NgffZarrConfig:
     # Rough memory limit in bytes
     memory_limit: int = default_memory_limit
-    cache_store: StoreLike = default_cache_store
+    cache_store: StoreLike = field(default_factory=default_store_factory)
 
 config = NgffZarrConfig()
