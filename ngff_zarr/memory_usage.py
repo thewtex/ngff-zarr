01f627b109dbb0d8ecd583093a0f9abe9d06a864
Matt McCormick <matt.mccormick@kitware.com>
2023-04-01 13:21:04 -0400

    ENH: Add memory_usage function

---
+++ ngff_zarr/memory_usage.py
@@ -0,0 +1,20 @@
+from typing import Set
+from .ngff_image import NgffImage
+
+from rich import print
+
+def memory_usage(image: NgffImage, constrained_dims: Set[str] = set()) -> int:
+    """Approximate array partition task graph memory usage in bytes.
+
+    Assumes tasks will have the same memory usage as an array chunk."""
+    arr = image.data
+    dims = image.dims
+    chunk = [c[0] for c in arr.chunks]
+    chunksize = 1
+    for dim in range(arr.ndim):
+        if dims[dim] in constrained_dims:
+            continue
+        chunksize *= chunk[dim] * arr.itemsize
+    ntasks = len(arr.dask)
+    return ntasks*chunksize
+
---
+++ test/test_memory_usage.py
@@ -0,0 +1,32 @@
+import dask.array
+import numpy as np
+import zarr
+
+from ngff_zarr import memory_usage, NgffImage, to_ngff_image, to_multiscales, to_ngff_zarr, from_ngff_zarr
+
+from rich import print
+
+def test_memory_usage():
+    arr = np.random.randint(0, 255, size=(4,4,4), dtype=np.uint8)
+    arr = dask.array.from_array(arr, chunks=2)
+    image = to_ngff_image(arr)
+    multiscales = to_multiscales(image, scale_factors=[], chunks=2)
+    store = zarr.storage.MemoryStore()
+    to_ngff_zarr(store, multiscales)
+    multiscales = from_ngff_zarr(store)
+
+    image = multiscales.images[0]
+    arr = image.data
+    assert arr.nbytes == 64
+    usage = memory_usage(image)
+    assert usage == 72
+    usage = memory_usage(image, {'z'})
+    assert usage == 36
+
+    arr1 = arr + 1
+    assert arr1.nbytes == 64
+    image.data = arr1
+    usage = memory_usage(image)
+    assert usage == 136
+    usage = memory_usage(image, {'z'})
+    assert usage == 68
