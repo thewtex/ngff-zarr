24c89a4ec2acbd0d0903a1dbf147b2106af055a4
Matt McCormick <matt.mccormick@kitware.com>
2023-07-16 17:08:03 -0400

    DOC: Add initial sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from datetime import date
+
+project = 'ngff-zarr'
+copyright = f'{date.today().year}, NumFOCUS'
+author = 'Matt McCormick'
+
+extensions = [
+    'sphinx.ext.autosummary',
+    'autodoc2',
+    'myst_parser',
+    'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
+    'sphinxext.opengraph',
+    'sphinx_design',
+]
+
+myst_enable_extensions = ["colon_fence", "fieldlist"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autodoc2_packages = [
+    {
+        "path": "../ngff_zarr",
+        "exclude_files": ["__about__.py"],
+    },
+]
+autodoc2_render_plugin = "myst"
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "itkwasm": ("https://itkwasm.readthedocs.io/en/latest/", None),
+    "dask": ("https://docs.dask.org/en/stable/", None),
+}
+
+html_theme = 'furo'
+html_static_path = ['_static']
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.png"
+html_title = f"{project}"
+
+# Furo options
+html_theme_options = {
+    "top_of_page_button": "edit",
+    "source_repository": "https://github.com/thewtex/ngff-zarr",
+    "source_branch": "main",
+    "source_directory": "docs",
+}
---
+++ docs/index.md
@@ -0,0 +1,48 @@
+ngff-zarr
+=========
+
+[![PyPI - Version](https://img.shields.io/pypi/v/ngff-zarr.svg)](https://pypi.org/project/ngff-zarr)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/ngff-zarr.svg)](https://pypi.org/project/ngff-zarr)
+[![Test](https://github.com/thewtex/ngff-zarr/actions/workflows/test.yml/badge.svg)](https://github.com/thewtex/ngff-zarr/actions/workflows/test.yml)
+[![DOI](https://zenodo.org/badge/541840158.svg)](https://zenodo.org/badge/latestdoi/541840158)
+
+A lean and kind Open Microscopy Environment (OME) Next Generation File Format (NGFF) Zarr implementation.
+
+## Features
+
+- Minimal dependencies
+- Work with arbitrary Zarr store types
+- Lazy, parallel, and web ready -- no local filesystem required
+- Process extremely large datasets
+- Multiple downscaling methods
+- Supports Python>=3.7
+- Implements version 0.4 of the [OME-Zarr
+NGFF specification](https://github.com/ome/ngff)
+
+## Installation
+
+::::{tab-set}
+
+:::{tab-item} System
+```shell
+pip install 'ngff-zarr[cli]'
+```
+:::
+
+:::{tab-item} Browser
+In Pyodide, e.g. the [Pyodide REPL](https://pyodide.org/en/stable/console.html) or [JupyterLite](https://jupyterlite.readthedocs.io/en/latest/try/lab),
+
+```python
+import micropip
+await micropip.install('ngff-zarr')
+:::
+
+::::
+
+```{toctree}
+:hidden:
+:maxdepth: 3
+:caption: 📖 Reference
+
+apidocs/index.rst
+```
