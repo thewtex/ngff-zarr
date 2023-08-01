1cb6fcd5aee5ccab0de6ea7dee4d5eb98c410ab9
Matt McCormick <matt.mccormick@kitware.com>
2023-08-01 15:55:48 -0400

    STYLE: task_count.py ruff checks

--- ngff_zarr/task_count.py
+++ ngff_zarr/task_count.py
@@ -1,10 +1,12 @@
-from typing import Set
+from typing import Optional, Set
 
 from .ngff_image import NgffImage
 
 
-def task_count(image: NgffImage, constrained_dims: Set[str] = set()) -> int:
+def task_count(image: NgffImage, constrained_dims: Optional[Set[str]] = None) -> int:
     """Approximate dask array partition task count."""
+    if constrained_dims is None:
+        constrained_dims = set()
     arr = image.data
     dims = image.dims
     slices = []
