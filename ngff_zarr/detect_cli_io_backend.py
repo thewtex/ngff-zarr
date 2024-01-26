f2a57cacc69db1b464dbca8ebee1f3932a538795
Matt McCormick <matt.mccormick@kitware.com>
2024-01-25 22:35:43 -0500

    PERF: Use itkwasm-image-io in the CLI

--- ngff_zarr/detect_cli_io_backend.py
+++ ngff_zarr/detect_cli_io_backend.py
@@ -6,6 +6,7 @@ from typing import List
 conversion_backends = [
     ("NGFF_ZARR", "ngff_zarr"),
     ("ZARR_ARRAY", "zarr"),
+    ("ITKWASM", "itkwasm_image_io"),
     ("ITK", "itk"),
     ("TIFFFILE", "tifffile"),
     ("IMAGEIO", "imageio"),
@@ -24,15 +25,58 @@ def detect_cli_io_backend(input: List[str]) -> ConversionBackend:
     if extension in ngff_zarr_supported_extensions:
         return ConversionBackend.NGFF_ZARR
 
-    itk_supported_extensions = (
+    itkwasm_supported_extensions = (
         ".bmp",
         ".dcm",
         ".gipl",
+        ".gipl.gz",
         ".hdf5",
         ".jpg",
         ".jpeg",
         ".iwi",
         ".iwi.cbor",
+        ".iwi.cbor.zst",
+        ".lsm",
+        ".mnc",
+        ".mnc.gz",
+        ".mnc2",
+        ".mgh",
+        ".mhz",
+        ".mha",
+        ".mhd",
+        ".mrc",
+        ".nia",
+        ".nii",
+        ".nii.gz",
+        ".hdr",
+        ".nrrd",
+        ".nhdr",
+        ".png",
+        ".pic",
+        ".vtk",
+        ".aim",
+        ".isq",
+        ".fdf",
+    )
+    if (
+        extension in itkwasm_supported_extensions
+        and len(input) == 1
+        and Path(input[0]).is_file()
+        and Path(input[0]).stat().st_size < 2e9
+    ):
+        return ConversionBackend.ITKWASM
+
+    itk_supported_extensions = (
+        ".bmp",
+        ".dcm",
+        ".gipl",
+        ".gipl.gz",
+        ".hdf5",
+        ".jpg",
+        ".jpeg",
+        ".iwi",
+        ".iwi.cbor",
+        ".iwi.cbor.zst",
         ".lsm",
         ".mnc",
         ".mnc.gz",
@@ -52,6 +96,7 @@ def detect_cli_io_backend(input: List[str]) -> ConversionBackend:
         ".pic",
         ".vtk",
         ".isq",  # Requires pip install itk-ioscanco,
+        ".aim",  # Requires pip install itk-ioscanco,
         ".fdf",  # Requires pip install itk-iofdf
     )
 
