a5a2c48570a089a1b5e2424aa9e8009a260b3d1a
Matt McCormick <matt@mmmccormick.com>
2024-11-19 06:53:29 -0500

    ENH: JSON-based deep equality for metadata in tests
    Avoid false-positive testing baseline comparisons.

--- test/_data.py
+++ test/_data.py
@@ -1,11 +1,13 @@
 import sys
 from pathlib import Path
+import json
 
 import pooch
 import pytest
 from itkwasm_image_io import imread
 from ngff_zarr import itk_image_to_ngff_image, to_ngff_zarr
 from zarr.storage import DirectoryStore, MemoryStore
+from deepdiff import DeepDiff
 
 test_data_ipfs_cid = "bafybeif6s65ezzpejhcj5366nwvyljfcj4brmefqtjxymbrcicbo3ggcei"
 test_data_sha256 = "c70c024e79d3dd6ecff86b9da1367b761d5f157f5cdad529c1fe482d2bce699a"
@@ -54,17 +56,29 @@ def input_images():
 def store_equals(baseline_store, test_store):
     baseline_keys = set(baseline_store.keys())
     test_keys = set(test_store.keys())
+    json_keys = {".zmetadata", ".zattrs", ".zgroup"}
+
     if baseline_keys != test_keys:
         sys.stderr.write("test keys != baseline keys\n")
         sys.stderr.write(f"baseline - test: {baseline_keys.difference(test_keys)}, \n")
         sys.stderr.write(f"test - baseline: {test_keys.difference(baseline_keys)}, \n")
         return False
     for k in baseline_keys:
-        if baseline_store[k] != test_store[k]:
-            sys.stderr.write(f"test value != baseline value for key {k}\n")
-            sys.stderr.write(f"baseline: {baseline_store[k]}, \n")
-            sys.stderr.write(f"test: {test_store[k]}, \n")
-            return False
+        if k in json_keys:
+            baseline_metadata = json.loads(baseline_store[k].decode("utf-8"))
+            test_metadata = json.loads(test_store[k].decode("utf-8"))
+
+            diff = DeepDiff(baseline_metadata, test_metadata, ignore_order=True)
+            if diff:
+                sys.stderr.write("Metadata in {k} files do not match\n")
+                sys.stderr.write(f"Differences: {diff}\n")
+                return False
+        else:
+            if baseline_store[k] != test_store[k]:
+                sys.stderr.write(f"test value != baseline value for key {k}\n")
+                sys.stderr.write(f"baseline: {baseline_store[k]}, \n")
+                sys.stderr.write(f"test: {test_store[k]}, \n")
+                return False
     return True
 
 
