938d705abec77db564339a657ec211955acbc13e
Matt <m.b.smith-5@umcutrecht.nl>
2025-05-02 21:20:09 +0200

    Added a failing test and the code to make it not fail.

---
+++ test/test_pyramid_integrity.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import dask.array as da
+from ngff_zarr import Multiscales, to_multiscales, NgffImage
+
+def test_checkPyramid():
+    """
+        When creating downscaled versions of images, subsequent resolutions were
+        not correctly being calculated. This test checks that the image volume
+        occupies the same space, and it checks that the scale factors are consistent
+        with the provided scale factors.
+    """
+    data = da.zeros(shape=(512, 512))
+    image = NgffImage(
+        data=data,
+        dims=["y", "x"],
+        scale={"y": 0.25, "x": 0.25},
+        translation={"y": 0, "x": 0},
+    )
+    scale_factors = [2, 4, 8, 16, 32]
+    multiscales: Multiscales = to_multiscales(
+        image,
+        scale_factors=scale_factors,
+    )
+    dims = image.data.shape
+    
+    sf = -1
+    for image in multiscales.images:
+        if sf < 0:
+            sf += 1
+            continue
+        scales = [image.scale[k] for k in image.scale]
+        print(scales, image.data.shape)
+        for i, d in enumerate(dims):
+            assert d * 0.25 == scales[i]*image.data.shape[i]
+            assert scales[i] == scale_factors[sf] * 0.25
+        sf += 1
+        
+test_checkPyramid()
