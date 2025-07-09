727f132f787b11c0a287e4a00a85b0da9ef03e4a
Matt McCormick <matt@fideus.io>
2025-07-08 22:29:36 -0400

    fix(ts): remove ITK_* and DASK_IMAGE_* methods

--- ts/src/types/methods.ts
+++ ts/src/types/methods.ts
@@ -2,11 +2,6 @@ export enum Methods {
   ITKWASM_GAUSSIAN = "itkwasm_gaussian",
   ITKWASM_BIN_SHRINK = "itkwasm_bin_shrink",
   ITKWASM_LABEL_IMAGE = "itkwasm_label_image",
-  ITK_GAUSSIAN = "itk_gaussian",
-  ITK_BIN_SHRINK = "itk_bin_shrink",
-  DASK_IMAGE_GAUSSIAN = "dask_image_gaussian",
-  DASK_IMAGE_MODE = "dask_image_mode",
-  DASK_IMAGE_NEAREST = "dask_image_nearest",
 }
 
 export const methodsValues = Object.values(Methods);
