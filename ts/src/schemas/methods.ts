d7bd8cb64c2b661de928c052d01bcce90eec3728
Matt McCormick <matt@fideus.io>
2025-07-09 22:46:21 -0400

    fix(ts): remove invalid methods from MethodsSchema

--- ts/src/schemas/methods.ts
+++ ts/src/schemas/methods.ts
@@ -4,9 +4,4 @@ export const MethodsSchema = z.enum([
   "itkwasm_gaussian",
   "itkwasm_bin_shrink",
   "itkwasm_label_image",
-  "itk_gaussian",
-  "itk_bin_shrink",
-  "dask_image_gaussian",
-  "dask_image_mode",
-  "dask_image_nearest",
 ]);
